713c606e21a16ff07b2bb45b13a3b5cb16a5d769
Abraham Hdru <abraham.hdru@gmail.com>
2023-04-06 11:19:10 +0200

    Changes to propthrilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import './App.css'
 // import Post from "./components/post/post";
@@ -6,11 +6,24 @@ import PostList from "./components/PostList/PostList";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+    const [modalIsVisible, setModalIsVisible] = useState(true);
+
+    function hideModalHandler(event: React.MouseEvent<HTMLButtonElement>) {
+        console.log("hideModalHandler is being called");
+        setModalIsVisible(false);
+    }
+
+    function showModalHandler() {
+        setModalIsVisible(true);
+    }
     return (
         <>
-            <MainHeader />
+            <MainHeader onCreatePost={showModalHandler} />
             <main>
-                <PostList />
+                <PostList
+                    isPosting={modalIsVisible}
+                    onStopPosting={hideModalHandler}
+                />
             </main>
         </>
     );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -5,19 +5,9 @@ import classes from "./PostList.module.css";
 import NewPost from "../NewPost/NewPost";
 import Modal from "../Modal/Modal";
 
-function PostList() {
-    const [modalIsVisible, setModalIsVisible] = useState(true);
+function PostList({ isPosting, onStopPosting }) {
     const [enteredBody, setEnteredBody] = useState("");
     const [enteredAuthor, setEnteredAuthor] = useState("");
-
-    function hideModalHandler(event: React.MouseEvent<HTMLButtonElement>) {
-        console.log("hideModalHandler is being called");
-        setModalIsVisible(false);
-    }
-
-    function showModalHandler() {
-        setModalIsVisible(true);
-    }
 
     function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
         setEnteredBody(event.target.value);
@@ -34,14 +24,16 @@ function PostList() {
             <button className={classes.closeButton} onClick={hideModalHandler}>
                 X
             </button>
-
-            <Modal isOpen={modalIsVisible} onClick={showModalHandler}>
-                <NewPost
-                    onBodyChange={changeBodyHandler}
-                    onAuthorChange={changeAuthorHandler}
-                    enteredBody={enteredBody}
-                />
-            </Modal>
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                    {/* isOpen={modalIsVisible} onClick={showModalHandler} */}
+                    <NewPost
+                        onBodyChange={changeBodyHandler}
+                        onAuthorChange={changeAuthorHandler}
+                        enteredBody={enteredBody}
+                    />
+                </Modal>
+            )}
 
             <ul className={classes.posts}>
                 <Post richPerson={enteredAuthor} companyName={enteredBody} />
