9a316ad0c75ac6809c901a6673788ec450358abe
Abraham Hdru <abraham.hdru@gmail.com>
2023-04-12 12:19:41 +0100

    Fixed create Post modal bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import './App.css'
-// import Post from "./components/post/post";
 import PostList from "./components/PostList/PostList";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-    const [modalIsVisible, setModalIsVisible] = useState(true);
+    const [isPosting, setIsPosting] = useState(true);
 
     function hideModalHandler(event: React.MouseEvent<HTMLButtonElement>) {
         console.log("hideModalHandler is being called");
-        setModalIsVisible(false);
+        setIsPosting(false);
     }
 
     function showModalHandler() {
-        setModalIsVisible(true);
+        setIsPosting(true);
     }
     return (
         <>
             <MainHeader onCreatePost={showModalHandler} />
             <main>
                 <PostList
-                    isPosting={modalIsVisible}
+                    isPosting={isPosting}
                     onStopPosting={hideModalHandler}
                 />
             </main>
