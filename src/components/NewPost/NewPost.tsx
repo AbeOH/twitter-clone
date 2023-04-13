72fd9095f8134ea991e62a405e83c7bf3bef7c9d
Abraham Hdru <abraham.hdru@gmail.com>
2023-04-13 14:25:54 +0100

    Post Submit Form

--- src/components/NewPost/NewPost.tsx
+++ src/components/NewPost/NewPost.tsx
@@ -1,43 +1,73 @@
 import classes from "./NewPost.module.css";
+import { useState } from "react";
 
 interface NewPostProps {
-    onBodyChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
-    enteredBody: string;
-    onAuthorChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    // onBodyChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    // enteredBody: string;
+    // onAuthorChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
     onCancel: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    // onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
 function NewPost({
-    onBodyChange,
-    enteredBody,
-    onAuthorChange,
+    // enteredBody,
     onCancel,
-}: NewPostProps) {
-    return (
-        <form className={classes.form}>
-            <p>
-                <label htmlFor="body">Text</label>
-                <textarea id="body" rows={3} onChange={onBodyChange} />
-            </p>
-            {/* <p>{props.enteredBody}</p> */}
-            <p>
-                <label htmlFor="name">Your name</label>
-                <input
-                    type="text"
-                    id="name"
-                    required
-                    onChange={onAuthorChange}
-                />
-            </p>
-            <p className={classes.actions}>
-                <button type="button" onClick={onCancel}>
-                    {" "}
-                    Cancel
-                </button>
-                <button> Submit</button>
-            </p>
-        </form>
-    );
+}: // onChange,
+NewPostProps) {
+    const [enteredBody, setEnteredBody] = useState("");
+    const [enteredAuthor, setEnteredAuthor] = useState("");
+
+    function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setEnteredBody(event.target.value);
+    }
+
+    function changeAuthorHandler(
+        event:
+            | React.ChangeEvent<HTMLTextAreaElement>
+            | React.ChangeEvent<HTMLInputElement>
+    ) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    function submitHandler(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const postData = {
+            body: enteredBody,
+            author: enteredAuthor,
+        };
+        console.log(postData);
+        onCancel();
+
+        return (
+            <form className={classes.form} onSubmit={submitHandler}>
+                <p>
+                    <label htmlFor="body">Text</label>
+                    <textarea
+                        id="body"
+                        required
+                        rows={3}
+                        onChange={changeBodyHandler}
+                    />
+                </p>
+                {/* <p>{props.enteredBody}</p> */}
+                <p>
+                    <label htmlFor="name">Your name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        required
+                        onChange={changeAuthorHandler}
+                    />
+                </p>
+                <p className={classes.actions}>
+                    <button type="button" onClick={onCancel}>
+                        Cancel
+                    </button>
+                    <button> Submit</button>
+                </p>
+            </form>
+        );
+    }
 }
 
 export default NewPost;
