26e968fc205ffd2d66249fa3808cce09b86d0ad0
Abraham Hdru <abraham.hdru@gmail.com>
2023-04-14 16:56:22 +0100

    Fixed from render issue

--- src/components/NewPost/NewPost.tsx
+++ src/components/NewPost/NewPost.tsx
@@ -37,37 +37,37 @@ NewPostProps) {
         };
         console.log(postData);
         onCancel();
-
-        return (
-            <form className={classes.form} onSubmit={submitHandler}>
-                <p>
-                    <label htmlFor="body">Text</label>
-                    <textarea
-                        id="body"
-                        required
-                        rows={3}
-                        onChange={changeBodyHandler}
-                    />
-                </p>
-                {/* <p>{props.enteredBody}</p> */}
-                <p>
-                    <label htmlFor="name">Your name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        required
-                        onChange={changeAuthorHandler}
-                    />
-                </p>
-                <p className={classes.actions}>
-                    <button type="button" onClick={onCancel}>
-                        Cancel
-                    </button>
-                    <button> Submit</button>
-                </p>
-            </form>
-        );
     }
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <p>
+                <label htmlFor="body">Text</label>
+                <textarea
+                    id="body"
+                    required
+                    rows={3}
+                    onChange={changeBodyHandler}
+                />
+            </p>
+            {/* <p>{props.enteredBody}</p> */}
+            <p>
+                <label htmlFor="name">Your name</label>
+                <input
+                    type="text"
+                    id="name"
+                    required
+                    onChange={changeAuthorHandler}
+                />
+            </p>
+            <p className={classes.actions}>
+                <button type="button" onClick={onCancel}>
+                    Cancel
+                </button>
+                <button> Submit</button>
+            </p>
+        </form>
+    );
 }
 
 export default NewPost;
