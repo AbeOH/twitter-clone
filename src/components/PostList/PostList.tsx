4d62cccadf14e5f12de9bc2f3bf33d7d2e60492b
Abraham Hdru <abraham.hdru@gmail.com>
2023-04-17 13:02:23 +0100

    Conditonal sign for no-post yet

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -37,12 +37,23 @@ function PostList({ isPosting, onStopPosting, postData }: PostListProps) {
                     />
                 </Modal>
             )}
-
-            <ul className={classes.posts}>
-                {posts.map((post) => (
-                    <Post author={post.author} body={post.body} />
-                ))}
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => (
+                        <Post
+                            key={post.body}
+                            author={post.author}
+                            body={post.body}
+                        />
+                    ))}
+                </ul>
+            )}
+            {posts.length === 0 && (
+                <div style={{ textAlign: "center", color: "white" }}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Be the first to post!</p>
+                </div>
+            )}
         </>
     );
 }
